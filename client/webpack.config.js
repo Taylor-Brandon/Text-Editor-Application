ff231114608803bfeae17257c9eb05ac702d7365
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-10-25 17:50:13 -0400

    added webpack html plugin, inject manifest plugin, and webpack manifest plugin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,7 +15,19 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        title: 'Hot Module Reloading',
+        template: './index.html',
+      }),
+      new InjectManifest({
+        swSrc: './src/sw.js',
+        swDest: 'service-worker.js',
+      }), 
+      new WebpackPwaManifest({
+        name: 'Text-Editor-Application',
+        short_name: 'Text-App',
+        description: 'Text Editor PWA',
+      }),
     ],
 
     module: {
@@ -26,6 +35,16 @@ module.exports = () => {
         {
           test: /\.css$/i,
           use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
         },
       ],
     },
